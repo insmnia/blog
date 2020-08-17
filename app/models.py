2597a08a9241c350736deddc277a1e70c00fba76
Блонский Ярослав <teamspirit3209@gmail.com>
2020-08-17 12:34:14 +0300

    new structure

--- app/models.py
+++ app/models.py
@@ -1,47 +1,47 @@
 from datetime import datetime
-from app import db, login, app
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
 from hashlib import md5
 from time import time
+from flask import current_app
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
+from app import db, login
 
 
-followers = db.Table('followers',
+followers = db.Table(
+    'followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
 
 
-class User(UserMixin,db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
-    last_seen = db.Column(db.DateTime,default=datetime.utcnow)
-
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     followed = db.relationship(
-        'User',secondary=followers,
+        'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
-        backref=db.backref('followers',lazy='dynamic'),
-        lazy='dynamic'
-    )
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
-    def set_password(self,password: str) -> None:
-        self.password_hash = generate_password_hash(password)
-    
-    def check_password(self,password: str) -> bool:
-        return check_password_hash(self.password_hash,password)
-
-    def __repr__(self) -> str:
+    def __repr__(self):
         return '<User {}>'.format(self.username)
 
-    def avatar(self,size: int) -> str:
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
     def follow(self, user):
         if not self.is_following(user):
@@ -54,7 +54,7 @@ class User(UserMixin,db.Model):
     def is_following(self, user):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
-        
+
     def followed_posts(self):
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
@@ -62,18 +62,25 @@ class User(UserMixin,db.Model):
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
-    def get_reset_password_token(self,expires_in=600):
+    def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
-            {'reset_password':self.id,'exp':time()+expires_in},
-            app.config['SECRET_KEY'],algorithm='HS256').decode('utf-8')
-        
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            current_app.config['SECRET_KEY'],
+            algorithm='HS256').decode('utf-8')
+
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token,app.config['SECRET_KEY'],algorithms=['HS256'])['reset_password']
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
         except:
             return
         return User.query.get(id)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 class Post(db.Model):
@@ -83,9 +90,5 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     language = db.Column(db.String(5))
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return '<Post {}>'.format(self.body)
-
-@login.user_loader
-def load_user(id: int) -> int:
-    return User.query.get(int(id))
--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
@@ -7,12 +10,12 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = "smtp.googlemail.com"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = 1
-    MAIL_USERNAME = "teamspirit3209@gmail.com"
-    MAIL_PASSWORD = "2787831ra"
-    ADMINS = ['studyformcbp1906@gmail.com']
-    MS_TRANSLATOR_KEY = "faeeafc8032f48ee91662aa0e8b2cc3b"
-    POSTS_PER_PAGE=25
-    LANGUAGES = ['en','es','ru']
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
+    LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    POSTS_PER_PAGE = 25
