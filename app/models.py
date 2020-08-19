fc888e64af036f8a2773d82ded3cec429a8b071d
Блонский Ярослав <teamspirit3209@gmail.com>
2020-08-19 11:19:15 +0300

    Pre-final commit

--- app/models.py
+++ app/models.py
@@ -1,12 +1,14 @@
-from datetime import datetime
+from datetime import datetime,timedelta
 from hashlib import md5
 from time import time
-from flask import current_app
+from flask import current_app, url_for
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 from app import db, login
 import json
+import base64
+import os
 
 followers = db.Table(
     'followers',
@@ -15,7 +17,28 @@ followers = db.Table(
 )
 
 
-class User(UserMixin, db.Model):
+class PaginatedAPIMixin(object):
+    @staticmethod
+    def to_collection_dict(query,page,per_page,endpoint,**kwargs):
+        resources = query.paginate(page,per_page,False)
+        data = {
+            'items':[item.to_dict() for item in resources.items],
+            '_meta':{
+                'page':page,
+                'per_page':per_page,
+                'total_pages':resources.pages,
+                "total_items":resources.total
+            },
+            '_links':{
+                'self':url_for(endpoint,page=page,per_page=per_page,**kwargs),
+                'next':url_for(endpoint,page=page+1,per_page=per_page,**kwargs) if resources.has_next else None,
+                'prev':url_for(endpoint,page=page-1,per_page=per_page,**kwargs) if resources.has_prev else None
+
+            }
+        }
+        return data
+
+class User(PaginatedAPIMixin,UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -28,6 +51,8 @@ class User(UserMixin, db.Model):
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    token = db.Column(db.String(32),index=True,unique=True)
+    token_expiration = db.Column(db.DateTime)
 
     messages_sent = db.relationship('Message',
                                     foreign_keys='Message.sender_id',
@@ -88,6 +113,45 @@ class User(UserMixin, db.Model):
         db.session.add(n)
         return n
 
+    def to_dict(self,include_email=False):
+        data = {
+            'id':self.id,
+            'username':self.username,
+            'last_seen':self.last_seen.isoformat()+'Z',
+            'about_me':self.about_me,
+            'post_count':self.posts.count(),
+            'follower_count':self.followers.count(),
+            'followed_count':self.followed.count(),
+            '_links':{
+                'self':url_for('api.get_user',id=self.id),
+                'followers':url_for('api.get_followers',id=self.id),
+                'followed':url_for('api.get_followed',id=self.id),
+                'avatar':self.avatar(64)
+            }
+        }
+        if include_email:
+            data['email'] = self.email
+        return data
+
+    def from_dict(self,data,new_user=False):
+        for field in ['username','email','about_me']:
+            if field in data:
+                setattr(self,field,data[field])
+        if new_user and 'password' in data:
+            self.set_password(data['password'])
+
+    def get_token(self,expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now+timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
     @staticmethod
     def verify_reset_password_token(token):
         try:
@@ -96,6 +160,13 @@ class User(UserMixin, db.Model):
         except:
             return
         return User.query.get(id)
+    
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
 
 
 @login.user_loader
